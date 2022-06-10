aa4fc3f5a36423e15dc1e0c4cec729eca61abe6c
Peter Heisig <peter@heisig.rocks>
2022-06-10 10:40:39 +0000

    fix(tests): revert router to dynamic loading

--- assets/status-tap.2ba59f75.js
+++ assets/status-tap.2ba59f75.js
@@ -1,3 +1,3 @@
-import{aF as a,aG as i,aH as c}from"./vendor.801380ed.js";/*!
+import{ar as a,as as i,at as c}from"./index.03df7e20.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const l=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=n.closest("ion-content");t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await t.scrollToTop(300),t.style.removeProperty("--overflow")})})})})};export{l as startStatusTap};
