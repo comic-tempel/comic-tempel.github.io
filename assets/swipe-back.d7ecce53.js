b70f1245734b4142b6947bb7e590e60a5958dd35
Peter Heisig <peter@heisig.rocks>
2022-06-10 10:01:34 +0000

    feat(contributions): Manga editor auto focus

--- assets/swipe-back.d7ecce53.js
+++
@@ -1,3 +0,0 @@
-import{aw as f,ax as D,ay as M}from"./vendor.4c1cb8ec.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const k=(o,h,m,g,p)=>{const r=o.ownerDocument.defaultView,c=f(o),w=t=>{const{startX:e}=t;return c?e>=r.innerWidth-50:e<=50},i=t=>c?-t.deltaX:t.deltaX,X=t=>c?-t.velocityX:t.velocityX;return D({el:o,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:t=>w(t)&&h(),onStart:m,onMove:t=>{const e=i(t)/r.innerWidth;g(e)},onEnd:t=>{const n=i(t),e=r.innerWidth,s=n/e,a=X(t),y=e/2,l=a>=0&&(a>.2||n>y),u=(l?1-s:s)*e;let d=0;if(u>5){const v=u/Math.abs(a);d=Math.min(v,540)}p(l,s<=0?.01:M(0,s,.9999),d)}})};export{k as createSwipeBackGesture};
