b397dcfe441b720200d69772c6d40387836582f9
Peter Heisig <peter@heisig.rocks>
2022-07-11 15:22:53 +0000

    feat(manga): reduce cover size for now

--- assets/status-tap.8507a690.js
+++ assets/status-tap.8507a690.js
@@ -1,3 +1,3 @@
-import{aw as a,ax as i,ay as c}from"./index.b9bcba95.js";/*!
+import{aw as a,ax as i,ay as c}from"./index.eb32840e.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const l=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=n.closest("ion-content");t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await t.scrollToTop(300),t.style.removeProperty("--overflow")})})})})};export{l as startStatusTap};
--- assets/useMachine.17848338.js
+++ assets/useMachine.17848338.js
@@ -1,4 +1,4 @@
-import{O as ye,v as ge,P as me}from"./index.b9bcba95.js";/*! *****************************************************************************
+import{O as ye,v as ge,P as me}from"./index.eb32840e.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
