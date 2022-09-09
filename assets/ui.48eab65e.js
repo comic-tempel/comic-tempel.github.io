b62b22a8dcef0a302bcca376ce2268f21e283569
Peter Heisig <peter@heisig.rocks>
2022-09-09 14:05:36 +0000

    fix(test): vitest user event delay increased...

--- assets/ui.48eab65e.js
+++
@@ -1,1 +0,0 @@
-import{q as e}from"./index.2105b4e5.js";const c=async a=>{var r,t;a.duration=(r=a.duration)!=null?r:2e3,a.color=(t=a.color)!=null?t:"primary",await(await e.create(a)).present()};export{c as t};
--- assets/useMachine.8ad8092e.js
+++ assets/useMachine.8ad8092e.js
@@ -1,4 +1,4 @@
-import{O as ye,v as ge,P as me}from"./index.2105b4e5.js";/*! *****************************************************************************
+import{O as ye,v as ge,P as me}from"./index.c4984c2c.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
