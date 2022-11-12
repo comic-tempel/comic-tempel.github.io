e1ffb2a556f67a36ff98a28b02ad00658fb69ff4
Peter Heisig <peter@heisig.rocks>
2022-11-12 15:29:01 +0000

    cleanup

--- assets/useMachine.ecb54bd2.js
+++ assets/useMachine.ecb54bd2.js
@@ -1,4 +1,4 @@
-import{O as ye,v as ge,P as me}from"./index.567f1f1c.js";/*! *****************************************************************************
+import{O as ye,P as ge,a5 as me}from"./index.13e5300e.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
