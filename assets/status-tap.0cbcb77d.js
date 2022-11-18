48dc868d78560aebfec98546f37c241ca6d51272
Peter Heisig <peter@heisig.rocks>
2022-11-18 14:52:50 +0000

    feat(library): add/remove manga

--- assets/status-tap.0cbcb77d.js
+++ assets/status-tap.0cbcb77d.js
@@ -1,3 +1,3 @@
-import{ax as a,ay as i,az as c}from"./index.13e5300e.js";/*!
+import{ax as a,ay as i,az as c}from"./index.9679af6a.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const l=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=n.closest("ion-content");t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await t.scrollToTop(300),t.style.removeProperty("--overflow")})})})})};export{l as startStatusTap};
