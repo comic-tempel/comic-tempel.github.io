3bd54bf0c153484f6181cd39ca700c61c6d6f311
Peter Heisig <peter@heisig.rocks>
2022-11-24 09:53:31 +0000

    feat(list): cover grid

--- assets/MangaListItem.4ea76ca5.js
+++
@@ -1,1 +0,0 @@
-import{d as o,o as s,c as r,w as t,a as n,v as c,y as l,u as e,z as u,A as i}from"./index.a5661104.js";const f=o({props:{title:null},setup(a){return(p,d)=>(s(),r(e(i),{button:!0,detail:!0},{default:t(()=>[n(e(u),{class:"ion-text-wrap"},{default:t(()=>[c(l(a.title),1)]),_:1})]),_:1}))}});export{f as _};
