4ef107faec2f326118bf701b128448c88acfb038
Peter Heisig <peter@heisig.rocks>
2022-11-25 13:46:42 +0000

    feat(collections): hide collection name

--- assets/status-tap.d610e44a.js
+++ assets/status-tap.d610e44a.js
@@ -1,3 +1,3 @@
-import{aw as r,ax as i,ay as c,az as d,aA as l}from"./index.81a8c471.js";/*!
+import{ax as r,ay as i,az as c,aA as d,aB as l}from"./index.ed418bf1.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
