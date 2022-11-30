edbcea37249122a1a1f8d40be3febb033c22134f
Peter Heisig <peter@heisig.rocks>
2022-11-30 12:58:54 +0000

    feat: date of publication and last update

--- assets/useMachine.781ef6e4.js
+++ assets/useMachine.781ef6e4.js
@@ -1,4 +1,4 @@
-import{V as ye,W as ge,a6 as me}from"./index.53a6c98f.js";/*! *****************************************************************************
+import{V as ye,W as ge,a6 as me}from"./index.e331f6fa.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
