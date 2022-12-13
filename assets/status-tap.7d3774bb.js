d632ee7928fd12ebdc0d10a806d72cd8f8fe15a1
Peter Heisig <peter@heisig.rocks>
2022-12-13 14:08:44 +0000

    feat: bulk add to library via barcode

--- assets/status-tap.7d3774bb.js
+++ assets/status-tap.7d3774bb.js
@@ -1,3 +1,3 @@
-import{aC as r,aD as i,aE as c,aF as d,aG as l}from"./index.963656f1.js";/*!
+import{aF as r,aG as i,aH as c,aI as d,aJ as l}from"./index.2d6df163.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
--- assets/swipe-back.d589efe7.js
+++
@@ -1,3 +0,0 @@
-import{ax as f,ay as D,az as M}from"./index.963656f1.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const k=(o,h,m,g,p)=>{const r=o.ownerDocument.defaultView,c=f(o),X=t=>{const{startX:e}=t;return c?e>=r.innerWidth-50:e<=50},i=t=>c?-t.deltaX:t.deltaX,w=t=>c?-t.velocityX:t.velocityX;return D({el:o,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:t=>X(t)&&h(),onStart:m,onMove:t=>{const e=i(t)/r.innerWidth;g(e)},onEnd:t=>{const n=i(t),e=r.innerWidth,s=n/e,a=w(t),y=e/2,l=a>=0&&(a>.2||n>y),u=(l?1-s:s)*e;let d=0;if(u>5){const v=u/Math.abs(a);d=Math.min(v,540)}p(l,s<=0?.01:M(0,s,.9999),d)}})};export{k as createSwipeBackGesture};
