a0cbed374a537820736b555bff50b305b0b59b68
Peter Heisig <peter@heisig.rocks>
2023-02-20 15:16:27 +0000

    feat: basic collection details

--- assets/status-tap.7d9ecab7.js
+++ assets/status-tap.7d9ecab7.js
@@ -1,3 +1,3 @@
-import{aK as r,aL as i,aM as c,aN as d,aO as l}from"./index.181d1343.js";/*!
+import{aO as r,aP as i,aQ as c,aR as d,aS as l}from"./index.462cec1b.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
