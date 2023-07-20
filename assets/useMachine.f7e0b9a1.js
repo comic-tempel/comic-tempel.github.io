b8570e8c9dc94717a99b2378258e86b2788559cf
Peter Heisig <peter@heisig.rocks>
2023-07-20 07:07:19 +0000

    🧹 chore: update CNAME
    wiki.comic-tempel.de

--- assets/useMachine.f7e0b9a1.js
+++ assets/useMachine.f7e0b9a1.js
@@ -1,4 +1,4 @@
-import{ag as ye,$ as ge,ay as me}from"./index.450d10d0.js";/*! *****************************************************************************
+import{ag as ye,$ as ge,ay as me}from"./index.f72ef724.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
